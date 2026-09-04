a9ac787d8970af4d0467eed8e6485985a81e8a67
sivwen <83945000+sivwen@users.noreply.github.com>
2026-09-05 01:02:28 +0900

    Extend Harmony transpiler stubs for v3.1

--- _tmp/ElonaLuckForElinV3/refs/Harmony/Stub.cs
+++ _tmp/ElonaLuckForElinV3/refs/Harmony/Stub.cs
@@ -1,1 +1,16 @@
-using System;namespace HarmonyLib{public sealed class Harmony{public Harmony(string id){}public void PatchAll(){}public void UnpatchSelf(){}}[AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple=true)]public sealed class HarmonyPatch:Attribute{public HarmonyPatch(Type t,string n){}public HarmonyPatch(Type t,string n,Type[] a){}}}
+using System;
+using System.Reflection.Emit;
+namespace HarmonyLib{
+ public sealed class Harmony{public Harmony(string id){}public void PatchAll(){}public void UnpatchSelf(){}}
+ [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple=true)]
+ public sealed class HarmonyPatch:Attribute{
+  public HarmonyPatch(Type t,string n){}
+  public HarmonyPatch(Type t,string n,Type[] a){}
+ }
+ public class CodeInstruction{
+  public OpCode opcode;
+  public object operand;
+  public CodeInstruction(OpCode opcode){this.opcode=opcode;}
+  public CodeInstruction(OpCode opcode,object operand){this.opcode=opcode;this.operand=operand;}
+ }
+}

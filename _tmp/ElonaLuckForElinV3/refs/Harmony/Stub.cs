17f5490746dda02a3039bd62968a58db075a59d8
sivwen <83945000+sivwen@users.noreply.github.com>
2026-09-05 10:40:37 +0900

    Add per-class Harmony processor stub for v3.3.1

--- _tmp/ElonaLuckForElinV3/refs/Harmony/Stub.cs
+++ _tmp/ElonaLuckForElinV3/refs/Harmony/Stub.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Reflection.Emit;
+using System;using System.Reflection;using System.Reflection.Emit;
 namespace HarmonyLib{
- public sealed class Harmony{public Harmony(string id){}public void PatchAll(){}public void UnpatchSelf(){}}
- [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple=true)]
- public sealed class HarmonyPatch:Attribute{public HarmonyPatch(){}
-  public HarmonyPatch(Type t,string n){}
-  public HarmonyPatch(Type t,string n,Type[] a){}
- }
- public class CodeInstruction{
-  public OpCode opcode;
-  public object operand;
-  public CodeInstruction(OpCode opcode){this.opcode=opcode;}
-  public CodeInstruction(OpCode opcode,object operand){this.opcode=opcode;this.operand=operand;}
- }
+ public sealed class Harmony{public Harmony(string id){}public PatchClassProcessor CreateClassProcessor(Type t)=>new PatchClassProcessor();public void UnpatchSelf(){}}
+ public sealed class PatchClassProcessor{public void Patch(){}}
+ [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple=true)]public sealed class HarmonyPatch:Attribute{public HarmonyPatch(){}public HarmonyPatch(Type t,string n){}public HarmonyPatch(Type t,string n,Type[] a){}}
+ public class CodeInstruction{public OpCode opcode;public object operand;public CodeInstruction(OpCode opcode){this.opcode=opcode;}public CodeInstruction(OpCode opcode,object operand){this.opcode=opcode;this.operand=operand;}}
 }
